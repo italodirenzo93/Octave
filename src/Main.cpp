893f0815c7ca34869c206261671833bb60d1a5e9
Italo Di Renzo <italodirenzo93@gmail.com>
2023-01-22 00:34:49 -0500

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,66 @@
+#include "CommonInclude.hpp"
+#include "graphics/Renderer.hpp"
+
+static constexpr int kDefaultWidth = 1280;
+static constexpr int kDefaultHeight = 720;
+
+int main() {
+    GLFWwindow* window = nullptr;
+
+    // Initialize GLFW library
+    if (!glfwInit()) {
+        printf( "Failed to initialize GLFW... exiting\n" );
+        return EXIT_FAILURE;
+    }
+
+    // Set window hints
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#else
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+#endif
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE );
+#ifdef __DEBUG__
+    glfwWindowHint( GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE );
+#endif
+
+    // Create window
+    window = glfwCreateWindow( kDefaultWidth, kDefaultHeight, "My Game Engine",
+                               nullptr, nullptr );
+    if (!window) {
+        printf( "Failed to create GLFW window... exiting\n" );
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent( window );
+
+    // Initialize Open GL extension loader
+    if (!gladLoadGLLoader(
+        reinterpret_cast<GLADloadproc>(glfwGetProcAddress) )) {
+        printf(
+            "Failed set proc address for Open GL extension loader... exiting\n" );
+        return EXIT_FAILURE;
+    }
+
+    graphics::Initialize();
+
+    glfwSetFramebufferSizeCallback(
+        window, []( GLFWwindow*, int width, int height ) {
+            graphics::ResizeFramebuffer( width, height );
+        } );
+
+    // Main loop
+    while (!glfwWindowShouldClose( window )) {
+        graphics::Clear( true, 0.1f, 0.1f, 0.1f );
+
+        glfwPollEvents();
+        glfwSwapBuffers( window );
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
---
+++ src/graphics/Renderer.cpp
@@ -0,0 +1,123 @@
+#include "Renderer.hpp"
+#include "CommonInclude.hpp"
+
+namespace graphics {
+#ifdef __DEBUG__
+void APIENTRY DebugOutputCallback( GLenum source, GLenum type, unsigned int id,
+                                   GLenum severity, GLsizei length,
+                                   const char* message,
+                                   const void* userParam ) {
+    // ignore non-significant error/warning codes
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
+        return;
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:
+            std::cout << "Source: API";
+            break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            std::cout << "Source: Window System";
+            break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            std::cout << "Source: Shader Compiler";
+            break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+            std::cout << "Source: Third Party";
+            break;
+        case GL_DEBUG_SOURCE_APPLICATION:
+            std::cout << "Source: Application";
+            break;
+        case GL_DEBUG_SOURCE_OTHER:
+            std::cout << "Source: Other";
+            break;
+    }
+    std::cout << std::endl;
+
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+            std::cout << "Type: Error";
+            break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            std::cout << "Type: Deprecated Behaviour";
+            break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            std::cout << "Type: Undefined Behaviour";
+            break;
+        case GL_DEBUG_TYPE_PORTABILITY:
+            std::cout << "Type: Portability";
+            break;
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            std::cout << "Type: Performance";
+            break;
+        case GL_DEBUG_TYPE_MARKER:
+            std::cout << "Type: Marker";
+            break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:
+            std::cout << "Type: Push Group";
+            break;
+        case GL_DEBUG_TYPE_POP_GROUP:
+            std::cout << "Type: Pop Group";
+            break;
+        case GL_DEBUG_TYPE_OTHER:
+            std::cout << "Type: Other";
+            break;
+    }
+    std::cout << std::endl;
+
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+            std::cout << "Severity: high";
+            break;
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            std::cout << "Severity: medium";
+            break;
+        case GL_DEBUG_SEVERITY_LOW:
+            std::cout << "Severity: low";
+            break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            std::cout << "Severity: notification";
+            break;
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+}
+#endif
+
+void Initialize() {
+#ifdef __DEBUG__
+    // Configure debug callback if we got a debug context
+    int gl_context_flags;
+    glGetIntegerv( GL_CONTEXT_FLAGS, &gl_context_flags );
+    if (gl_context_flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+        glEnable( GL_DEBUG_OUTPUT );
+        glEnable( GL_DEBUG_OUTPUT_SYNCHRONOUS );
+        glDebugMessageCallback( DebugOutputCallback, nullptr );
+        glDebugMessageControl( GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0,
+                               nullptr, GL_TRUE );
+    }
+#endif
+
+    glEnable( GL_DEPTH_TEST );
+    glEnable( GL_STENCIL_TEST );
+    glEnable( GL_CULL_FACE );
+}
+
+void ResizeFramebuffer( int width, int height ) {
+    glViewport( 0, 0, width, height );
+}
+
+void Clear( bool depth, float r, float g, float b, float a ) {
+    int clear_flags = GL_COLOR_BUFFER_BIT;
+
+    if (depth) {
+        clear_flags |= GL_DEPTH_BUFFER_BIT;
+        glClearDepthf( 1.0f );
+    }
+
+    glClearColor( r, g, b, a );
+    glClear( clear_flags );
+}
+}
