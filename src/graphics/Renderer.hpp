90f9a9c7dd38db154970c48960d117cee2daf367
Italo Di Renzo <italodirenzo93@gmail.com>
2023-01-22 16:41:08 -0500

    Print renderer info string on startup

--- src/graphics/Renderer.hpp
+++ src/graphics/Renderer.hpp
@@ -1,11 +1,15 @@
 #ifndef RENDERER_HPP
 #define RENDERER_HPP
 
+#include "CommonInclude.hpp"
+
 namespace graphics {
 void Initialize();
 void ResizeFramebuffer( int width, int height );
 void Clear( bool depth = true, float r = 0.0f, float g = 0.0f, float b = 0.0f,
             float a = 1.0f );
+std::string GetRendererInfo();
+
 }
 
 #endif
