e9c09dce4d5b64eb1a0ff4f3897ce77068764f8a
Italo Di Renzo <italo.direnzo@testdouble.com>
2023-01-23 12:20:49 -0500

    Use config file template and defaults

--- resources/shaders/light.frag
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 FragColor;
-
-uniform vec3 uLightColor = vec3(1.0);
-
-void main() {
-	FragColor = vec4(uLightColor, 1.0);
-}
--- src/Config.cpp
+++ src/Config.cpp
@@ -2,33 +2,87 @@
 
 #include <SimpleIni.h>
 
-#include <iostream>
+#include "CommonInclude.hpp"
 
 using namespace std;
-
-static constexpr const char* kConfigFile = "./resources/config/engine.ini";
+namespace fs = std::filesystem;
 
 namespace config {
+static constexpr const char* kConfigTemplate = "resources/config/Engine.ini";
+
+// Video
+static constexpr int kDefaultWidth = 800;
+static constexpr int kDefaultHeight = 600;
+static constexpr bool kDefaultFullscreen = false;
+static constexpr int kDefaultSyncInterval = 1;
+
+// Renderer
+static constexpr const char* kDefaultShaderList =
+    "basic,mesh,lit,single_color,skybox";
+static constexpr const char* kDefaultShaderDirectory = "resources/shaders";
+static constexpr bool kDefaultPreloadShaders = false;
+
 static CSimpleIniA ini;
 
 inline bool EnsureKeyExists( const string& section, const string& key ) {
-    if (!ini.KeyExists( section.c_str(), key.c_str() )) {
+    if ( !ini.KeyExists( section.c_str(), key.c_str() ) ) {
         cout << "Configuration value [" << section << "]/" << key
-            << " not found" << endl;
+             << " not found" << endl;
         return false;
     }
 
     return true;
 }
 
-#define ENSURE_KEY_EXISTS( section, key ) if ( !EnsureKeyExists( section, key ) ) return false
+#define ENSURE_KEY_EXISTS( section, key ) \
+    if ( !EnsureKeyExists( section, key ) ) return false
+
+static void SetDefaultConfig( CSimpleIniA& config ) {
+    config.SetLongValue( "Video", "FramebufferWidth", kDefaultWidth );
+    config.SetLongValue( "Video", "FramebufferHeight", kDefaultHeight );
+    config.SetBoolValue( "Video", "IsFullscreen", kDefaultFullscreen );
+    config.SetLongValue( "Video", "SyncInterval", kDefaultSyncInterval );
+
+    config.SetValue( "Renderer", "ShaderList", kDefaultShaderList );
+    config.SetValue( "Renderer", "ShaderDirectory", kDefaultShaderDirectory );
+    config.SetBoolValue( "Renderer", "PreloadShaders", kDefaultPreloadShaders );
+}
 
 bool Initialize() {
-    const SI_Error result = ini.LoadFile( kConfigFile );
-    if (result < 0) {
-        cout << "Could not find " << kConfigFile << "..." << endl;
-        return false;
-    }
+    SI_Error result;
+
+    static constexpr const char* configFile = "Engine.ini";
+
+    // Check for config file
+    if ( fs::exists( configFile ) ) {
+        // Try to load it
+        result = ini.LoadFile( configFile );
+        if ( result < 0 ) {
+            cout << "Could not read configuration file... using default "
+                    "configuration"
+                 << endl;
+            SetDefaultConfig( ini );
+        }
+    } else {
+        // Try to copy template
+        if ( !fs::copy_file( kConfigTemplate, configFile ) ) {
+            // Could not copy template, set built-in defaults
+            SetDefaultConfig( ini );
+            result = ini.SaveFile( configFile );
+            if ( result < 0 ) {
+                cout << "Could not write default configuration file..." << endl;
+            }
+        } else {
+            result = ini.LoadFile( configFile );
+            if ( result < 0 ) {
+                cout << "Could not read configuration file... using default "
+                        "configuration"
+                     << endl;
+                SetDefaultConfig( ini );
+            }
+        }  // copy file
+    }      // config file exists
+
     assert( result == SI_OK );
 
     return true;
@@ -39,7 +93,8 @@ void Reset() { ini.Reset(); }
 bool TryGetInt( const string& section, const string& key, int& value ) {
     ENSURE_KEY_EXISTS( section, key );
 
-    value = static_cast<int>(ini.GetLongValue( section.c_str(), key.c_str() ));
+    value =
+        static_cast<int>( ini.GetLongValue( section.c_str(), key.c_str() ) );
 
     return true;
 }
@@ -60,4 +115,4 @@ bool TryGetBool( const std::string& section, const std::string& key,
 
     return true;
 }
-} // namespace config
+}  // namespace config
