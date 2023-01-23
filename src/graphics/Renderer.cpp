7d39127c2a9c4440fbb34130d09f68305296b519
Italo Di Renzo <italodirenzo93@gmail.com>
2023-01-23 01:05:54 -0500

    More config

--- src/graphics/Renderer.cpp
+++ src/graphics/Renderer.cpp
@@ -1,5 +1,6 @@
 #include "Renderer.hpp"
 #include "ShaderManager.hpp"
+#include "Config.hpp"
 
 #include <sstream>
 
@@ -90,7 +91,79 @@ void APIENTRY DebugOutputCallback( GLenum source, GLenum type, unsigned int id,
 }
 #endif
 
-void Initialize() {
+// Default width, height
+static constexpr int kDefaultWidth = 1280;
+static constexpr int kDefaultHeight = 720;
+
+// Open GL window
+static GLFWwindow* g_window = nullptr;
+
+bool Initialize() {
+    // Initialize GLFW library
+    if (!glfwInit()) {
+        cout << "Failed to initialize GLFW... exiting" << endl;
+        return false;
+    }
+
+    // Set window hints
+#ifdef __APPLE__
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
+#else
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+#endif
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE );
+#ifdef __DEBUG__
+    glfwWindowHint( GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE );
+#endif
+
+    int width, height;
+    if (!config::TryGetInt( "Video", "FramebufferWidth", width )) {
+        width = kDefaultWidth;
+    }
+    if (!config::TryGetInt( "Video", "FramebufferHeight", height )) {
+        height = kDefaultHeight;
+    }
+
+    int sync_interval;
+    if (!config::TryGetInt( "Video", "SyncInterval", sync_interval )) {
+        sync_interval = 1;
+    }
+
+    // Create window
+    g_window =
+        glfwCreateWindow( width, height, "My Game Engine", nullptr, nullptr );
+    if (!g_window) {
+        cout << "Failed to create GLFW window... exiting" << endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent( g_window );
+    glfwSwapInterval( sync_interval );
+
+    glfwSetFramebufferSizeCallback( g_window,
+                                    []( GLFWwindow*, int width, int height ) {
+                                        glViewport( 0, 0, width, height );
+                                    } );
+
+    // Initialize Open GL extension loader
+    if (!gladLoadGLLoader(
+        reinterpret_cast<GLADloadproc>(glfwGetProcAddress) )) {
+        cout
+            << "Failed set proc address for Open GL extension loader... exiting"
+            << endl;
+        return EXIT_FAILURE;
+    }
+
+    // Always enabled
+    glEnable( GL_DEPTH_TEST );
+    glEnable( GL_CULL_FACE );
+
+    glClearDepthf( 1.0f );
+
 #ifdef __DEBUG__
     // Configure debug callback if we got a debug context
     int gl_context_flags;
@@ -104,20 +177,35 @@ void Initialize() {
     }
 #endif
 
-    // Always enabled
-    glEnable( GL_DEPTH_TEST );
-    glEnable( GL_CULL_FACE );
-
-    glClearDepthf( 1.0f );
-
     // Because Open GL coordinates are weird...
     stbi_set_flip_vertically_on_load( true );
 
-	ShaderManager::Instance().PreloadShaders();
+    // Shader pre-cache
+    {
+        bool preload = false;
+        config::TryGetBool( "Renderer", "PreloadShaders", preload );
+
+        if (preload) {
+            ShaderManager::Instance().PreloadShaders();
+        }
+    }
+
+    return true;
+}
+
+void Terminate() {
+    if (g_window) {
+        glfwDestroyWindow( g_window );
+    }
+    glfwTerminate();
 }
 
 void ResizeFramebuffer( int width, int height ) {
     glViewport( 0, 0, width, height );
+}
+
+bool IsWindowOpen() {
+    return g_window != nullptr && !glfwWindowShouldClose( g_window );
 }
 
 void Clear( bool depth, float r, float g, float b, float a ) {
@@ -125,11 +213,17 @@ void Clear( bool depth, float r, float g, float b, float a ) {
 
     if (depth) {
         clear_flags |= GL_DEPTH_BUFFER_BIT;
-        glClearDepthf( 1.0f );
     }
 
     glClearColor( r, g, b, a );
     glClear( clear_flags );
+}
+
+void Present() {
+    glfwPollEvents();
+    if (g_window) {
+        glfwSwapBuffers( g_window );
+    }
 }
 
 std::string GetRendererInfo() {
@@ -140,8 +234,7 @@ std::string GetRendererInfo() {
         << "GLSL Version: " << glGetString( GL_SHADING_LANGUAGE_VERSION )
         << endl
         << "GPU Vendor: " << glGetString( GL_VENDOR ) << endl
-        << "GPU Model: " << glGetString( GL_RENDERER ) << endl
-        << endl;
+        << "GPU Model: " << glGetString( GL_RENDERER ) << endl;
 
     return oss.str();
 }
