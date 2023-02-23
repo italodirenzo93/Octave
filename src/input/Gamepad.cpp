42341907ff93a1b2c30948f2375c58c6ab9e8fb9
Italo Di Renzo <italodirenzo93@gmail.com>
2023-02-23 12:01:12 -0500

    Gamepad class

---
+++ src/input/Gamepad.cpp
@@ -0,0 +1,52 @@
+#include "Gamepad.hpp"
+
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+namespace octave::input {
+
+Gamepad::Gamepad( int player_index ) noexcept : player_index_( player_index ) {
+	assert( glfwJoystickPresent( player_index ) );
+	assert( glfwJoystickIsGamepad( player_index ) );
+}
+
+std::string Gamepad::GetName() const noexcept {
+	return glfwGetGamepadName( player_index_ );
+}
+
+std::tuple<float, float> Gamepad::GetLeftStick() const noexcept {
+	GLFWgamepadstate state;
+	if ( !glfwGetGamepadState( player_index_, &state ) ) {
+		return make_tuple( 0.0f, 0.0f );
+	}
+
+	return make_tuple( state.axes[GLFW_GAMEPAD_AXIS_LEFT_X],
+					   state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y] );
+}
+
+std::tuple<float, float> Gamepad::GetRightStick() const noexcept {
+	GLFWgamepadstate state;
+	if ( !glfwGetGamepadState( player_index_, &state ) ) {
+		return make_tuple( 0.0f, 0.0f );
+	}
+
+	return make_tuple( state.axes[GLFW_GAMEPAD_AXIS_RIGHT_X],
+					   state.axes[GLFW_GAMEPAD_AXIS_RIGHT_Y] );
+}
+
+bool Gamepad::IsButtonDown( int button ) const noexcept {
+	GLFWgamepadstate state;
+	if ( !glfwGetGamepadState( player_index_, &state ) ) {
+		return false;
+	}
+
+	return state.buttons[button] == GLFW_PRESS;
+}
+
+bool Gamepad::IsPresent( int player_index ) noexcept {
+	return glfwJoystickPresent( player_index ) &&
+		   glfwJoystickIsGamepad( player_index );
+}
+
+}  // namespace octave::input
