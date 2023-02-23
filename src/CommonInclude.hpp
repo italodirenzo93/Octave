3a4613cb48441447913e45b3bdf809cd0f49716a
Italo Di Renzo <italodirenzo93@gmail.com>
2023-02-23 12:01:11 -0500

    Add GL and AL ThrowIfFailed functions

--- src/CommonInclude.hpp
+++ src/CommonInclude.hpp
@@ -1,5 +1,5 @@
-#ifndef COMMONINCLUDE_HPP
-#define COMMONINCLUDE_HPP
+#ifndef OCTAVE_COMMONINCLUDE_HPP
+#define OCTAVE_COMMONINCLUDE_HPP
 
 // clang-format off
 
@@ -31,6 +31,8 @@
 // C headers
 #include <cstdint>
 
+#include "Exception.hpp"
+
 #define NON_COPYABLE_CLASS( className )     \
 	className( const className& ) = delete; \
 	className& operator=( const className& ) = delete;
@@ -45,5 +47,82 @@
 
 #define SELF_REFERENCE_CHECK( other ) \
 	if ( &other == this ) return *this
+
+namespace octave::graphics::gl {
+
+class OpenGLError : public Exception {
+public:
+	explicit OpenGLError( GLenum error ) noexcept {
+		switch ( error ) {
+			case GL_INVALID_ENUM:
+				message_ = "GL_INVALID_ENUM";
+				break;
+			case GL_INVALID_VALUE:
+				message_ = "GL_INVALID_VALUE";
+				break;
+			case GL_INVALID_OPERATION:
+				message_ = "GL_INVALID_OPERATION";
+				break;
+			case GL_STACK_OVERFLOW:
+				message_ = "GL_STACK_OVERFLOW";
+				break;
+			case GL_STACK_UNDERFLOW:
+				message_ = "GL_STACK_UNDERFLOW";
+				break;
+			case GL_OUT_OF_MEMORY:
+				message_ = "GL_OUT_OF_MEMORY";
+				break;
+			default:
+				message_ = "Unknown error";
+				break;
+		}
+	}
+};
+
+inline void ThrowIfFailed() {
+	const GLenum error = glGetError();
+	if ( error != GL_NO_ERROR ) {
+		throw OpenGLError( error );
+	}
+}
+
+}  // namespace octave::graphics::gl
+
+namespace octave::audio::al {
+
+class OpenALError : public Exception {
+public:
+	explicit OpenALError( ALenum error ) noexcept {
+		switch ( error ) {
+			case AL_INVALID_NAME:
+				message_ = "AL_INVALID_NAME";
+				break;
+			case AL_INVALID_ENUM:
+				message_ = "AL_INVALID_ENUM";
+				break;
+			case AL_INVALID_VALUE:
+				message_ = "AL_INVALID_VALUE";
+				break;
+			case AL_INVALID_OPERATION:
+				message_ = "AL_INVALID_OPERATION";
+				break;
+			case AL_OUT_OF_MEMORY:
+				message_ = "AL_OUT_OF_MEMORY";
+				break;
+			default:
+				message_ = "Unknown error";
+				break;
+		}
+	}
+};
+
+inline void ThrowIfFailed() {
+	const ALenum error = alGetError();
+	if ( error != AL_NO_ERROR ) {
+		throw OpenALError( error );
+	}
+}
+
+}  // namespace octave::audio::al
 
 #endif
--- src/audio/Buffer.cpp
+++ src/audio/Buffer.cpp
@@ -4,11 +4,35 @@ namespace octave::audio {
 
 Buffer::Buffer() {
 	alGenBuffers( 1, &id_ );
-	// throw if error
+	al::ThrowIfFailed();
 }
 
 Buffer::~Buffer() noexcept {
 	alDeleteBuffers( 1, &id_ );
+}
+
+int Buffer::GetFrequency() const noexcept {
+	int frequency;
+	alGetBufferi( id_, AL_FREQUENCY, &frequency );
+	return frequency;
+}
+
+int Buffer::GetBitDepth() const noexcept {
+	int bit_depth;
+	alGetBufferi( id_, AL_BITS, &bit_depth );
+	return bit_depth;
+}
+
+int Buffer::GetChannels() const noexcept {
+	int channels;
+	alGetBufferi( id_, AL_CHANNELS, &channels );
+	return channels;
+}
+
+int Buffer::GetSize() const noexcept {
+	int size;
+	alGetBufferi( id_, AL_SIZE, &size );
+	return size;
 }
 
 }  // namespace octave::audio
