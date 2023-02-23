19ef37a64d7b683f5e7ab05c1018ed501602cb01
Italo Di Renzo <italodirenzo93@gmail.com>
2023-02-23 12:01:10 -0500

    Clean up and use the camera class

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -8,30 +8,39 @@ namespace octave {
 
 class Camera {
 public:
-    Camera( float aspect_ratio, const glm::vec3& pos, const glm::vec3& up );
+    Camera() noexcept;
     ~Camera() = default;
 
-    float field_of_view_ = 45.0f;
-    float aspect_ratio_;
+    [[nodiscard]] glm::mat4 GetProjectionMatrix() const noexcept { return matrix_projection_; }
+    [[nodiscard]] glm::mat4 GetViewMatrix() const noexcept { return matrix_view_; }
+    [[nodiscard]] glm::vec3 GetPosition() const noexcept { return position_; };
+    [[nodiscard]] float GetFieldOfView() const noexcept { return field_of_view_; }
+    [[nodiscard]] float GetAspectRatio() const noexcept { return aspect_ratio_; }
 
-    glm::vec3 position_;
-    glm::vec3 front_;
-    glm::vec3 up_;
-    glm::vec3 right_;
-    glm::vec3 world_up_;
-
-    float pitch_ = 0.0f;
-    float yaw_ = -90.0f;
-
-    [[nodiscard]] glm::mat4 GetProjectionMatrix() const;
-    [[nodiscard]] glm::mat4 GetViewMatrix() const;
-
-    void Project( const graphics::Shader& shader ) const;
-    void Unproject( const graphics::Shader& shader ) const;
-
-    void Update();
+    Camera& SetPosition( float x, float y, float z) noexcept;
+    Camera& SetPosition( const glm::vec3& position ) noexcept;
+    Camera& SetFieldOfView( float fov ) noexcept;
+    Camera& SetAspectRatio( float aspect ) noexcept;
 
 private:
+     glm::vec3 position_;
+     glm::vec3 front_;
+     glm::vec3 up_;
+     glm::vec3 right_;
+     const glm::vec3 world_up_ = glm::vec3(0.0f, 1.0f, 0.0f);
+
+     float field_of_view_;
+     float aspect_ratio_;
+
+     float pitch_ = 0.0f;
+     float yaw_ = -90.0f;
+
+     glm::mat4 matrix_projection_;
+     glm::mat4 matrix_view_;
+
+     void UpdateProjectionMatrix() noexcept;
+     void UpdateViewMatrix() noexcept;
+
     NON_COPYABLE_OR_MOVABLE_CLASS( Camera )
 };
 
