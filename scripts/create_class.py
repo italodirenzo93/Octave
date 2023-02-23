4297241381b433277a103465e9f6286dd39f695e
Italo Di Renzo <italodirenzo93@gmail.com>
2023-02-23 12:01:12 -0500

    Class creation helper script

---
+++ scripts/create_class.py
@@ -0,0 +1,87 @@
+from sys import argv
+from os import path
+
+class_name = argv[1]
+if not class_name:
+    raise "No class name provided. Exiting..."
+
+tokens = class_name.split("::")
+if len(tokens) > 1:
+    namespaces = tokens[:-1]
+    class_name = tokens[-1]
+else:
+    namespaces = ["octave"]
+
+namespace_str = "::".join(namespaces)
+
+# Header
+header_filename = f"{class_name}.hpp"
+
+header_code = f"""#ifndef OCTAVE_{class_name.upper()}_HPP
+#define OCTAVE_{class_name.upper()}_HPP
+
+namespace {namespace_str} {{
+
+class {class_name} {{
+public:
+\t// Constructors
+\t{class_name}() = default;
+\t{class_name}( const {class_name}& other ) = default;
+\t{class_name}( {class_name}&& other ) noexcept = default;
+\t~{class_name}() noexcept = default;
+
+\t// Operators
+\t{class_name}& operator=( const {class_name}& other ) = default;
+\t{class_name}& operator=( {class_name}&& other ) noexcept = default;
+
+\t// Getter/setter pairs
+
+\t// Methods
+
+\t// Fields
+
+protected:
+\t// Methods
+
+\t// Fields
+
+private:
+\t// Methods
+
+\t// Fields
+
+}};
+
+}}
+
+#endif
+
+"""
+
+# Source
+source_filename = f"{class_name}.cpp"
+
+source_code = f"""#include "{header_filename}"
+
+namespace {namespace_str} {{
+
+// Code...
+
+}}
+
+"""
+
+# Write files
+project_root = "./"
+path_parts = ["src", *namespaces[1:]]
+
+header_loc = path.join(project_root, *path_parts, header_filename)
+with open(path.realpath(header_loc), 'w') as header_file:
+    header_file.write(header_code)
+
+source_loc = path.join(project_root, *path_parts, source_filename)
+with open(path.realpath(source_loc), 'w') as source_file:
+    source_file.write(source_code)
+
+print(f"Created header file at {path.join(header_loc)}.")
+print(f"Created source file at {path.join(source_loc)}.")
