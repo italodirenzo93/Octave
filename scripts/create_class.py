7572699882a5ef723b01bd64a60c3e1311842bfe
Italo Di Renzo <italodirenzo93@gmail.com>
2023-02-24 21:09:44 -0500

    Modify create_class script to update build file

--- scripts/create_class.py
+++ scripts/create_class.py
@@ -1,11 +1,11 @@
-from sys import argv
-from os import path
+import os
+import sys
 
-class_name = argv[1]
+class_name = sys.argv[1]
 if not class_name:
     raise "No class name provided. Exiting..."
 
-class_path = path.split(class_name)
+class_path = os.path.split(class_name)
 class_name = class_path[-1]
 
 # Header
@@ -67,16 +67,23 @@ namespace Octave {{
 """
 
 # Write files
-project_root = "./"
-path_parts = ("src", *class_path[:1])
+project_root = ""
+path_parts = class_path[:1]
 
-header_loc = path.join(project_root, *path_parts, header_filename)
-with open(path.realpath(header_loc), 'w') as header_file:
+header_loc = os.path.join(project_root, "src", *path_parts, header_filename)
+with open(os.path.realpath(header_loc), 'w') as header_file:
     header_file.write(header_code)
 
-source_loc = path.join(project_root, *path_parts, source_filename)
-with open(path.realpath(source_loc), 'w') as source_file:
+source_loc = os.path.join(project_root, "src", *path_parts, source_filename)
+with open(os.path.realpath(source_loc), 'w') as source_file:
     source_file.write(source_code)
 
-print(f"Created header file at {path.join(header_loc)}.")
-print(f"Created source file at {path.join(source_loc)}.")
+print(f"Created header file at {os.path.join(header_loc)}.")
+print(f"Created source file at {os.path.join(source_loc)}.")
+
+# Update meson build file
+if os.system(f"meson rewrite target Octave add {'/'.join((*path_parts, source_filename))} {'/'.join((*path_parts, header_filename))}") != 0:
+    print("Error updating build file(s).")
+else:
+    print("Successfully updated build file(s).")
+    os.system("meson setup builddir --reconfigure")
