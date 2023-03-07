f63cf86a74e32137b47c2847c963d701816972b5
Italo Di Renzo <italodirenzo93@gmail.com>
2023-03-07 01:59:55 -0500

    Vertex colors

--- src/graphics/OpenGL/Pipeline.cpp
+++ src/graphics/OpenGL/Pipeline.cpp
@@ -1,6 +1,8 @@
 #include "pch/pch.hpp"
 #include "Pipeline.hpp"
 
+#include "core/Log.hpp"
+
 namespace Octave {
 
 Pipeline::Pipeline() {
@@ -18,11 +20,32 @@ void Pipeline::SetVertexShader( SharedRef<Shader> vertex_shader ) noexcept {
 	vertex_shader_ = std::move( vertex_shader );
 }
 
+void Pipeline::SetVertexUniformBuffer( SharedRef<Buffer> ubo, uint32_t index, uint32_t binding ) noexcept {
+	assert( binding < GL_MAX_UNIFORM_BUFFER_BINDINGS );
+
+	glUniformBlockBinding( vertex_shader_->GetApiResource(), index, binding );
+
+	glBindBufferBase( GL_UNIFORM_BUFFER, binding, ubo->GetApiResource() );
+
+	vs_ubos_[binding] = std::move( ubo );
+}
+
+
 void Pipeline::SetFragmentShader( SharedRef<Shader> fragment_shader ) noexcept {
 	glUseProgramStages( id_, GL_FRAGMENT_SHADER_BIT,
 						fragment_shader->GetApiResource() );
 
 	fragment_shader_ = std::move( fragment_shader );
+}
+
+void Pipeline::SetFragmentUniformBuffer( SharedRef<Buffer> ubo, uint32_t index, uint32_t binding ) noexcept {
+	assert( binding < GL_MAX_UNIFORM_BUFFER_BINDINGS );
+
+	glUniformBlockBinding( fragment_shader_->GetApiResource(), index, binding );
+
+	glBindBufferBase( GL_UNIFORM_BUFFER, binding, ubo->GetApiResource() );
+
+	fs_ubos_[binding] = std::move( ubo );
 }
 
 }  // namespace Octave
